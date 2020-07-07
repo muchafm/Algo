57c1a7eb815532478cafb5ce9b3cf497f65c4245
muchafm <sophie.roussel@knplabs.com>
2020-07-07 16:50:27 +0200

    wip

--- ex_03.php
+++ ex_03.php
@@ -1,18 +1,31 @@
 <?php
 
-$n = [1, 2, 5];
+$n = [1, 2, 5, 4];
 $m = [3, 5, 2];
 
-function getDifference($n, $m) {
-    $intersection = [];
-    foreach($n as $valuesOfN) {
-        foreach($m as $valuesOfM) {
-            if($valuesOfN !== $valuesOfM) {
-                $intersection[] = $valuesOfN;
-            } 
+function getDifference(array $n, array $m) {
+    $sizeArrayN = count($n);
+    $sizeArrayM = count($m);
+    $difference = [];
+    $same = [];
+    if($sizeArrayN > 0) {
+        for($i = 0; $i < $sizeArrayN; $i++) {
+            foreach($n as $Nvalues) {
+                foreach($m as $Mvalues) {
+                    if($Nvalues !== $Mvalues) {
+                        var_dump($Nvalues);
+                        $difference[] = $Nvalues;
+                        return $difference;
+                    } else {
+                        $same[] = $Nvalues;
+                    }
+                }
+            }
         }
     }
-    return $intersection;
+
+    // return $difference;
+
 }
 
 $res = getDifference($n, $m);
