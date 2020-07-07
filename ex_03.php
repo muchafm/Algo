ea6cd2a8ab840e9dd664aa9300b7b16458181ccb
muchafm <sophie.roussel@knplabs.com>
2020-07-07 17:51:48 +0200

    wip ex_03.php

--- ex_03.php
+++ ex_03.php
@@ -1,12 +1,13 @@
 <?php
 
-$n = [1,2,5,4];
-$m = [4,5,2,6,7];
+$n = [1,2,5,4,8];
+$m = [4,5,2,6,7,10];
 
 function getDifference(array $n, array $m) {
     $sizeArrayN = count($n);
+    $sizeArrayM = count($m);
     
-    if($sizeArrayN > 0) {
+    if($sizeArrayN > 0 && $sizeArrayM > 0) {
         $union = [];
         $difference = [];
         foreach($n as $nVal) {
@@ -16,7 +17,7 @@ function getDifference(array $n, array $m) {
                     $union[] = $mVal;
                     foreach($union as $values) {
                         if($values == $nVal) {
-                            $difference[] = $nVal;
+                            $difference[] = $values;
                         }
                     }
                 }
