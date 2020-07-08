8e2fb220a179125fc776bf7b233effbc8322390a
muchafm <sophie.roussel@knplabs.com>
2020-07-08 17:25:41 +0200

    ex3 done

--- ex_02.php
+++ ex_02.php
@@ -3,13 +3,13 @@
 $n = [1, 99, 101, 202];
 $m = [1, 2, 999];
 
-function getUnion($n, $m) {
+function getUnion(array $n, array $m): array {
     $union = [];
-    foreach($n as $valuesOfN) {
-        $union[] = $valuesOfN;
-        foreach($m as $valuesOfM) {
-            if(!in_array($valuesOfM, $union)) {
-                $union[] = $valuesOfM;
+    foreach($n as $nVal) {
+        $union[] = $nVal;
+        foreach($m as $mVal) {
+            if(!in_array($mVal, $union)) {
+                $union[] = $mVal;
             }
         }
     }
@@ -18,8 +18,9 @@ function getUnion($n, $m) {
 
 }
 
-function orderUnion($union) {
+function orderUnion(array $union): array {
     $size = count($union);
+
     for($i = 0; $i < $size; $i++) {
         $nbToMove = $union[$i];
         for($j = 0; $j < $i; $j++) {
@@ -29,6 +30,7 @@ function orderUnion($union) {
                 $nbToMove = $currentNb;
             }
         }
+
         $union[$i] = $nbToMove;
     }
     return $union;
--- ex_03.php
+++ ex_03.php
@@ -1,31 +1,22 @@
 <?php
 
-$n = [1,2,5,4,8];
-$m = [4,5,2,6,7,10];
+$n = [2,6,7,8];
+$m = [2,4,5,6,9];
 
-function getDifference(array $n, array $m) {
-    $sizeArrayN = count($n);
-    $sizeArrayM = count($m);
-    
-    if($sizeArrayN > 0 && $sizeArrayM > 0) {
-        $union = [];
-        $difference = [];
-        foreach($n as $nVal) {
-            $union[] = $nVal;
-            foreach($m as $mVal) {
-                if(!in_array($mVal, $union)) {
-                    $union[] = $mVal;
-                    foreach($union as $values) {
-                        if($values == $nVal) {
-                            $difference[] = $values;
-                        }
-                    }
-                }
+function getDifference(array $n, array $m): array {
+    $union = [];
+    foreach($n as $nVal) {
+        $union[] = $nVal;
+        foreach($m as $mVal) {
+            if(!in_array($mVal, $union)) {
+                $union[] = $mVal;
+            } else if($mVal !== $nVal) {
+                $difference[] = $nVal;
             }
         }
-
-        return $difference;
     }
+    $diff = array_unique($difference);
+    return $diff;
 }
 
 $res = getDifference($n, $m);
