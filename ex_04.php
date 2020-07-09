b2a06fbff9a952bd686a1c2380a2dd5abb1fc630
muchafm <sophie.roussel@knplabs.com>
2020-07-09 17:24:13 +0200

    ex4

---
+++ ex_04.php
@@ -0,0 +1,27 @@
+<?php
+
+$m = array(
+    array(1,2,3,4), 
+    array(2,1,2,3), 
+    array(3,2,1,2), 
+    array(4,3,2,1)
+);
+
+function getEvenNumbers(array $matrice) {
+    $evenNumbers = 0;
+    foreach($matrice as $matriceValues) {
+        foreach($matriceValues as $values) {
+            if($values % 2 == 0) {
+                $evenNumbers++;
+            }
+        }
+    }
+
+    return $evenNumbers;
+
+}
+
+
+
+$matchs = getEvenNumbers($m);
+echo $matchs;
