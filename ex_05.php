abbac4f9971c9ab9c497eb1a4bd94761c06309fe
muchafm <sophie.roussel@knplabs.com>
2020-07-10 14:57:11 +0200

    ex5

--- ex_05.php
+++ ex_05.php
@@ -1,1 +1,27 @@
 <?php
+
+function getMultiple() {
+    $multipleOfThree = 0;
+    $multipleOfFive = 0;
+    for($i = 1; $i < 1000; $i++) {
+        if($i % 3 == 0) {
+            $multipleOfThree+= $i;
+            $array[] = $i;
+        } else if ($i % 5 == 0) {
+            $multipleOfFive+= $i;
+            $array[] = $i;
+        }
+    }
+
+    $sumOfMultiple = 0;
+    
+    foreach($array as $values) {
+        $sumOfMultiple+= $values;
+    }
+    
+    return $sumOfMultiple;
+
+}
+
+$multiple = getMultiple();
+print_r($multiple);
