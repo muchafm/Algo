80828d4eddac2141ab12c0d91af4864a7e78305f
muchafm <sophie.roussel@knplabs.com>
2020-07-15 11:47:59 +0200

    fix

--- ex_05.php
+++ ex_05.php
@@ -1,27 +1,17 @@
 <?php
 
-function getMultiple() {
-    $multipleOfThree = 0;
-    $multipleOfFive = 0;
+function getMultiple(): int {
+    $sumOfMultiple = 0;
 
     for($i = 1; $i < 1000; $i++) {
         if($i % 3 == 0) {
-            $multipleOfThree+= $i;
-            $arrayOfMultiple[] = $i;
+            $sumOfMultiple += $i;
         } else if ($i % 5 == 0) {
-            $multipleOfFive+= $i;
-            $arrayOfMultiple[] = $i;
+            $sumOfMultiple += $i;
         }
     }
 
-    $sumOfMultiple = 0;
-
-    foreach($arrayOfMultiple as $values) {
-        $sumOfMultiple+= $values;
-    }
-    
     return $sumOfMultiple;
-
 }
 
 $multiple = getMultiple();
