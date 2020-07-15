61af6d62d6e11b3fa5c09e5e8a51d51af0f793c3
muchafm <sophie.roussel@knplabs.com>
2020-07-15 14:21:54 +0200

    fix

--- ex_05.php
+++ ex_05.php
@@ -1,12 +1,13 @@
 <?php
 
 function getMultiple(int $limit): int {
+    
     $sumOfMultiple = 0;
 
     for($i = 1; $i < $limit; $i++) {
-        if($i % 3 == 0) {
+        if($i % 3 === 0) {
             $sumOfMultiple += $i;
-        } else if ($i % 5 == 0) {
+        } else if ($i % 5 === 0) {
             $sumOfMultiple += $i;
         }
     }
